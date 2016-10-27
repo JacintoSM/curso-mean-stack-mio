d435f07bbe76dee05d5148a0564b8012147092c1
manana <manana>
2016-10-27 13:28:44 +0200

    cambios calculadora

--- calculadora-web/js/control.js
+++ calculadora-web/js/control.js
@@ -1,5 +1,5 @@
 var memoria;
-calculadora = new Calculadora();
+
 function agregarAlDisplay(valor){
     //TODO: Tengo que ingresar en el input el valor que me llega
     let pantalla;
@@ -12,33 +12,33 @@ function operacion(operador){
         //TODO: Almacenarlo en la memoria
     //TODO: en caso de que me llegue un igual
         // TODO: llamar a la calculadora con el valor de memoria, el valor del display 
-        calculadora.sumar(memoria,document.getElementById("display"));
+        sumar(memoria,document.getElementById("display"));
     if( "+"== operador){
       memoria = $("#display").eval;
       $("#display").html("   ");
       if($("#display").html =="="){
-          calculadora.sumar(memoria,document.getElementById("#display").eval);
+        sumar(memoria,document.getElementById("#display").eval);
       }
      } 
 if( "*"== operador){
       memoria = $("#display").eval;
       $("#display").html("  ");
       if($("#display").html =="="){
-          calculadora.multiplicar(memoria,document.getElementById("#display").eval);
+        multiplicar(memoria,document.getElementById("#display").eval);
       }
      } 
 if( "/"== operador){
       memoria = $("#display").eval;
       $("#display").html("   ");
       if($("#display").html =="="){
-          calculadora.dividir(memoria,document.getElementById("#display").eval);
+        dividir(memoria,document.getElementById("#display").eval);
       }
      } 
 if( "-"== operador){
       memoria = $("#display").eval;
       $("#display").html("   ");
       if($("#display").html =="="){
-          calculadora.restar(memoria,document.getElementById("#display").eval);
+        restar(memoria,document.getElementById("#display").eval);
       }
      } 
      
